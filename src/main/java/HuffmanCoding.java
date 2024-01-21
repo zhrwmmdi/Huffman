2bd51ad91de5e399076b7174c1ffaf766d374f3b
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-01-21 20:31:11 +0330

    Update on HuffmanCoding class: createHuffmanTree() method in created to form up the tree and return its root with help of a priority queue

--- src/main/java/HuffmanCoding.java
+++ src/main/java/HuffmanCoding.java
@@ -1,19 +1,41 @@
+import data_handler.FileReader;
+import structure.Node;
+
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class HuffmanCoding {
-    private final Map<Character, Integer> charMap = new HashMap<>();
+    private final Map<Character, Integer> charFrequencyMap = new HashMap<>();
+
+    //The double colon (::) operator, also known as method reference operator in Java, is used to call a method by referring to it with the help of its class directly.
+    private final PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(Node::getFrequency));
+    public void encode(String data){
+        Node root = createHuffmanTree(data);
+
+    }
+    public Node createHuffmanTree(String data){
+        fillMap(data);
+        //used to create and then return a 'set' of the same elements that are already present in the HashMap. Because in map case we can't have item.getKey() or item.getVAlue()
+        for (var item : charFrequencyMap.entrySet()){
+            queue.add(new Node(item.getKey(), item.getValue()));
+        }
+        while (queue.size() > 1) {
+            Node left = queue.poll();
+            Node right = queue.poll();
+
+            assert right != null;
+            int sumFreq = left.getFrequency() + right.getFrequency();
+
+            queue.add(new Node(sumFreq, left, right));
+        }
+        return queue.peek();
+    }
     private void fillMap(String data){
         for (char c:data.toCharArray()){
-            charMap.put(c,(charMap.getOrDefault(c,0)+1));
+            charFrequencyMap.put(c,(charFrequencyMap.getOrDefault(c,0)+1));
         }
     }
-
-    public void createHuffmanTree(String data){
-        fillMap(data);
-
-    }
-
-
 }
 
