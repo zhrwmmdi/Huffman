7a30bd18297761cc1f1546886435233179b04caf
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-01-25 13:45:07 +0330

    reducing code in main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,8 @@
-import Coding.HuffmanCoding;
-import Coding.HuffmanDecoding;
 import data_handler.Browser;
-import data_handler.FileReader;
+
 public class Main {
     public static void main(String[] args){
-        Browser browser  = new Browser();
-
-        FileReader reader = new FileReader();
-        reader.readFile(browser.getSelectedFilePath());
-
-        HuffmanCoding huffman = new HuffmanCoding();
-        huffman.encode(FileReader.getStringData());
-
-        HuffmanDecoding decoding = new HuffmanDecoding();
-        decoding.decode();
+            Browser browser  = new Browser();
+            browser.browse();
     }
 }
