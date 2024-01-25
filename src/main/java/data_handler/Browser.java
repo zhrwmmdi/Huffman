fcc2e4e9db985cee3dc8747443ddd94a6099c356
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-01-25 13:44:14 +0330

    update on browser class: function is determined according to the chosen file format + exit programm if cancel button is pressed + show warning dialog if the file format is not accepted

--- src/main/java/data_handler/Browser.java
+++ src/main/java/data_handler/Browser.java
@@ -1,5 +1,8 @@
 package data_handler;
 
+import coding.HuffmanDecoding;
+import coding.HuffmanEncoding;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
@@ -7,24 +10,48 @@ import java.io.File;
 public class Browser {
     private final JFrame frame;
     private String selectedFilePath;
+    private File selectedFile;
+
 
     public Browser() {
-        frame =  new JFrame("Huffman");
+        frame =  new JFrame();
         frame.setSize(400, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+    }
+    public void browse(){
         showFileChooser();
+        FileReader reader = new FileReader();
+        reader.readFile(this.getSelectedFilePath());
+        chooseFunction();
+    }
+    private void chooseFunction(){
+        if (selectedFile.getName().endsWith(".txt")){
+            HuffmanEncoding encoder = new HuffmanEncoding();
+            encoder.encode(FileReader.getStringData());
+        } else if (selectedFile.getName().endsWith(".com")) {
+            HuffmanDecoding decoding = new HuffmanDecoding();
+            decoding.decode();
+        }else {
+            JOptionPane.showMessageDialog(null,"Wrong file format.\nPlease choose a proper file.","Wrong Format",JOptionPane.WARNING_MESSAGE);
+            browse();
+        }
+        System.exit(0);
     }
 
     private void showFileChooser() {
         JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileFilter(new FileNameExtensionFilter("Text Files (*.txt)", "txt"));
-        int result = fileChooser.showOpenDialog(frame);
 
+        FileNameExtensionFilter txt = new FileNameExtensionFilter("Text Files (*.txt)", "txt");
+        FileNameExtensionFilter com = new FileNameExtensionFilter("Compressed Files (*.com)", "com");
+        fileChooser.setFileFilter(com);
+        fileChooser.setFileFilter(txt);
+
+        int result = fileChooser.showOpenDialog(frame);
         if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
+            selectedFile = fileChooser.getSelectedFile();
             selectedFilePath = selectedFile.getAbsolutePath();
-        }
+        }else System.exit(0);
     }
     public String getSelectedFilePath() {
         return selectedFilePath;
